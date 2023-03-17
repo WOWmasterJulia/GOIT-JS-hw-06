ba884f30ed4279fce4e617a1fa0e95a468727ab9
WOWmasterJulia <topazzz@ukr.net>
2023-03-18 00:15:01 +0200

    11

---
+++ js/task-01.js
@@ -0,0 +1,28 @@
+// const listWithId = document.querySelector('#categories');
+// console.log(listWithId);
+
+// const listWithClass = document.querySelector('.item');
+// console.log(listWithClass);
+
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
+
+
+
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
+
+// console.log(document);
+
+// const body = document.body;
+// console.log(body);
+
+// const list = body.firstElementChild;
+// console.log(list);
+
+// const firstListItem = list.firstElementChild;
+// console.log(firstListItem);
+
+const categories = #categories;
+console.log(listItems);
+const h2 = document.querySelector(".item__title")
