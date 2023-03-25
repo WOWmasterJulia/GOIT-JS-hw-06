c7a7bb6247086032247918d0d32992bf407f90ce
WOWmasterJulia <topazzz@ukr.net>
2023-03-26 00:44:08 +0200

    1

--- js/task-08.js
+++ js/task-08.js
@@ -36,39 +36,55 @@
 // +++++++++++++++ ВАРИАНТ 2 +++++++++++++++++
 // Global object +++++++++++++++
   
-const formEl = document.querySelector(".login-form");
-
-const result = {};
-formEl.addEventListener("submit", checkSubmit);
-function checkSubmit(event) {
-    event.preventDefault();
-   const {
-    elements: { email, password }
-    } = event.currentTarget; 
-    if (email.value.trim() === "" || password.value.trim() === "") {
-    return alert("Please fill in all the fields!");
-  }
-    result[email.name] = email.value.trim();
-    result[password.name] = password.value.trim();
-    event.currentTarget.reset();
-};
-console.log(result);
-
-
-// +++++++++++++++ ВАРИАНТ 3 +++++++++++++++++
-// Не учитывает пробел до и после введенного теекста +++++++++++++++
-
 // const formEl = document.querySelector(".login-form");
 
+// const result = {};
 // formEl.addEventListener("submit", checkSubmit);
 // function checkSubmit(event) {
 //     event.preventDefault();
 //    const {
 //     elements: { email, password }
 //     } = event.currentTarget; 
-//     if (email.value === "" || password.value === "") {
+//     if (email.value.trim() === "" || password.value.trim() === "") {
 //     return alert("Please fill in all the fields!");
 //   }
-//   console.log(`Еmail: ${email.value}, Password: ${password.value}`);
-//   event.currentTarget.reset();
+//     result[email.name] = email.value;
+//     result[password.name] = password.value;
+//     event.currentTarget.reset();
+// };
+// console.log(result);
+
+
+// +++++++++++++++ ВАРИАНТ 3 +++++++++++++++++
+// Не учитывает пробел до и после введенного теекста +++++++++++++++
+
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", checkSubmit);
+function checkSubmit(event) {
+    event.preventDefault();
+   const {
+    elements: { email, password }
+    } = event.currentTarget; 
+    if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  console.log(`Еmail: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
+
+// +++++++++++++++ ВАРИАНТ 4 +++++++++++++++++
+// const formEl = document.querySelector(".login-form");
+// formEl.addEventListener("submit", checkSubmit);
+
+// function checkSubmit(event) {
+//     event.preventDefault();
+//     const {
+//         elements: { email, password }
+//     } = event.currentTarget;
+//     if (email.value.trim() === "" || password.value.trim() === "") {
+//         return alert("Please fill in all the fields!");
+//     }
+//     console.log({ email:email.value, password:password.value })
+//     event.currentTarget.reset();
 // };
